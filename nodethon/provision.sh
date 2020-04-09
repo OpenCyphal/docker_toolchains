e8f2b79c384f691fd4f9687333c539c013a0b509
Nuno Marques <n.marques21@hotmail.com>
2020-04-09 18:50:50 +0100

    Update nodethon/provision.sh
    Co-Authored-By: Scott Dixon <dixonsco@amazon.com>

--- nodethon/provision.sh
+++ nodethon/provision.sh
@@ -39,8 +39,6 @@ apt-get -y install unzip
 # deadsnakes maintains a bunch of python versions for Ubuntu.
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
-apt-get -y install python3.5
-apt-get -y install python3.6
 apt-get -y install python3.7
 apt-get -y install python3.8
 apt-get -y install python3-pip
